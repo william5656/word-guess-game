1671ba6e3185a15cbf5043e290f40705f837e297
william zhu <william_zhu@hotmail.ca>
2018-06-08 12:56:22 -0400

    added the guessed word box

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,50 @@
     $(document).ready(function() {
 
+        var life = 12;
+        var lose = 0;
+        var usedWords = [];
         var anime = [
             "naruto",
-            "bleach"
+            "bleach",
+            "gto"
           ];
 
           var word = anime[Math.floor(Math.random() * anime.length)];
-          var answerArray = [];
+
           for (var i=0; i < word.length; i++){
             var underscore = $("<span>");  
-            underscore.addClass("underline");
-            underscore.attr("data-letter", word[i]);
+            underscore.addClass("underline underline-" + word[i]);
+            underscore.attr("data-letter" , word[i]);
               
               $(".box1").append(underscore);
           }   
 
-              document.onkeyup = function(event){
-                var userguess = event.key;
-                var letter = $("<div>");
-                for(i=0; j < word.length; j++){
-                letter.text($(this).attr("data-letter"))
-                }
-            }
-
+            document.onkeyup = function(event) {
+                var result = false;
                 
-             
-        
-    
+                var userguess = event.key;
+                for(var j=0; j < word.length; j++){ 
+                    if (userguess === word[j]){
+                        $(".underline-" + word[j]).text(word[j]);
+                        result = true;
+                    }
+                }
+                if (window.event.keyCode == 116) {
+                    life = life+0
+                }
+                if (result === false){
+                    life = life-1;
+                    $(".life").text(life);
+                    usedWords.push(userguess);
+                    $(".box").append(userguess);
 
+                }
+                if (life === 0){
+                
+                    lose = lose +1;
+                    $("h4").text(lose); 
+                }
+              };
+    
           
             })
